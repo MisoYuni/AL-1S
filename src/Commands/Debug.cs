545d64e9e96b170a43271391461c8c287dd89764
MisonoYuni <me@yuni.st>
2023-05-11 13:28:08 +0100

    Add explanation of the command group & Refactor response method

--- src/Commands/Debug.cs
+++ src/Commands/Debug.cs
@@ -1,22 +1,18 @@
 ﻿// Debug.cs
 
-using DSharpPlus;
-using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using Serilog;
 
 namespace Alice.Commands
 {
+    [SlashCommandGroup("debug", "A group of commands for debugging purposes")]
     public class Debug : ApplicationCommandModule
     {
         [SlashCommand("ping", "Ping")]
         internal async Task Ping(InteractionContext ctx)
         {
             Log.Information($"Command: Author={ctx.Member.Id}, Command=\'Ping\'");
-            await ctx.CreateResponseAsync(
-                InteractionResponseType.ChannelMessageWithSource,
-                new DiscordInteractionResponseBuilder()
-                .WithContent($"WebSocket: {ctx.Client.Ping}ms"));
+            await ctx.CreateResponseAsync($"WebSocket: {ctx.Client.Ping}ms");
         }
     }
 }
