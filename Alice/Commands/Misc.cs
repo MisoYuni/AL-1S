e78c616508a182f64b9746755c5cc01f6ccc7cc0
MisoYuni <yuni@misono.io>
2023-05-26 21:08:11 +0000

    Specify variable types

--- Alice/Commands/Misc.cs
+++ Alice/Commands/Misc.cs
@@ -9,13 +9,14 @@ using Serilog;
 namespace Alice.Commands
 {
     [SlashCommandGroup("misc", "sus")]
-    public class Misc :ApplicationCommandModule
+    public class Misc : ApplicationCommandModule
     {
         [SlashCommand("someone", "Ping randomly selected user")]
         internal async Task Someone(InteractionContext ctx)
         {
-            var rand = new Random();
-            var userlist = ctx.Channel.Users;
+            Random rand = new Random();
+            IReadOnlyList<DiscordMember> userlist = ctx.Channel.Users;
+            
             await ctx.CreateResponseAsync(new DiscordInteractionResponseBuilder()
             .WithContent($"> Hi {userlist[rand.Next(userlist.Count)].Mention}")
             .AddMentions(Mentions.All));
